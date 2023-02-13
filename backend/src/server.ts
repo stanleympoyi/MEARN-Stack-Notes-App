04306728df647f39fc0506261119c4390566f457
Stanley Mpoyi <mpoyis@oregonstate.edu>
2023-02-13 13:21:49 -0600

    added database schema

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import env from "./util/validateEnv";
 const port = env.PORT;
 
 mongoose.connect(env.MONGO_CONNECTION_STRING).then(() => {
-  console.log("MongoDB is connected....");
+  console.log("MongoDB note app connected....");
 
   app.listen(port, () => {
     console.log("Server running on port: " + port);
